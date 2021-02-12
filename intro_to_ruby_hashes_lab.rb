621fbba5439b864ef0b14811181b3eb5183b80c8
Zecharias Mengis <zmengis@gmail.com>
2021-02-12 01:26:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,10 +41,10 @@ def monopoly_with_fourth_tier
     :three_pieces_owned => 100,
     :four_pieces_owned => 200 },
   :names => {
-    :reading_railroad => { :mortgage_value => },
-    :pennsylvania_railroad => {},
-    :b_and_o_railroad => {},
-    :shortline_railroad => {} }
+    :reading_railroad => { :mortgage_value => 100 },
+    :pennsylvania_railroad => { :mortgage_value => 200 },
+    :b_and_o_railroad => { :mortgage_value => 400 },
+    :shortline_railroad => { :mortgage_value => 800 } }
   }
   }
 
