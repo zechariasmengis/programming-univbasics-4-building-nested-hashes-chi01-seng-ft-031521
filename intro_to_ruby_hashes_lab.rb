71e2b093ff798d9a8cefe6197afd60558035019d
Zecharias Mengis <zmengis@gmail.com>
2021-02-12 01:21:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,10 +16,12 @@ end
 def monopoly_with_third_tier
   hash = { :railroads => {
     :pieces => 4,
-  :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200} :names => {
+  :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
+  }
+  :names => {
     
   }
-  }
+
   }
 
 hash
